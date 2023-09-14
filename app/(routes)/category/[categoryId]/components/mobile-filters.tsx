65b4917099f694382042dc734b2c229c2912406c
Lautaro <lautarofaridtoledo@gmail.com>
2023-09-14 03:58:59 -0300

    feat: category with filters

---
+++ app/(routes)/category/[categoryId]/components/mobile-filters.tsx
@@ -0,0 +1,19 @@
+import { Color, Size } from "@/types";
+
+interface MobileFiltersProps {
+    sizes: Size[];
+    colors: Color[];
+}
+
+const MobileFilters: React.FC<MobileFiltersProps> = ({
+    sizes,
+    colors
+}) => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+ 
+export default MobileFilters;
